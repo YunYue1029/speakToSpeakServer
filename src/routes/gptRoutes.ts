b6703e274b1f2a9e2cf10aad49c4196078ffef1a
鄭博允 <115069485+YunYue1029@users.noreply.github.com>
2025-03-15 22:45:11 +0800

    updateGPTFunction

--- src/routes/gptRoutes.ts
+++ src/routes/gptRoutes.ts
@@ -9,21 +9,65 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-router.post("/chat", async (req, res) => {
+router.post("/noSpeechWithImg", async (req, res) => {
   try {
-    const { message } = req.body;
+    const { model, userinfo, prompt, imageUrl, temperature } = req.body;
 
-    if (!message) {
-      res.status(400).json({ error: "請提供 message 參數" });
+    if (!model && !prompt && ! userinfo && !temperature && !imageUrl) {
+      res.status(400).json({ error: "請提供參數" });
       return;
     }
 
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: message }],
-      temperature: 0.7,
+      model: model,
+      messages: [
+        { role: "system", content: userinfo },
+        { role: "user", content: [
+          { type: "text", text: prompt },
+          { type: "image_url", image_url: { url: imageUrl },},
+        ],
+        },
+      ],
+      temperature: temperature || 0.5,
     });
-    res.json(response.choices[0].message);
+    res.json({
+      model : model,
+      userinfo : userinfo,
+      prompt : prompt,
+      temperature : temperature,
+      reply : response.choices[0]?.message?.content
+    });
+    return;
+  } catch (error) {
+    console.error("❌ GPT API 請求錯誤:", error);
+    res.status(500).json({ error: "GPT API 發生錯誤" });
+  }
+});
+
+router.post("/WithSpeechNoImg", async (req, res) => {
+  try {
+    const { model, userinfo, prompt, temperature } = req.body;
+
+    if (!model && !prompt && ! userinfo && !temperature) {
+      res.status(400).json({ error: "請提供參數" });
+      return;
+    }
+
+    const response = await openai.chat.completions.create({
+      model: model,
+      messages: [
+        { role: "system", content: userinfo },
+        { role: "user", content: prompt }
+      ],
+      temperature: temperature || 0.5,
+    });
+    res.json({
+      model : model,
+      userinfo : userinfo,
+      prompt : prompt,
+      temperature : temperature,
+      reply : response.choices[0]?.message?.content
+    });
     return;
   } catch (error) {
     console.error("❌ GPT API 請求錯誤:", error);
